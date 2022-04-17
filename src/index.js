a6c91de1a8e620b6f10c091417c03166d51e5b42
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-04-17 14:42:17 -0500

    Add CSS baseline

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { StyledEngineProvider } from '@mui/material';
 import './index.css';
 import App from './components/App';
+import CssBaseline from '@mui/material/CssBaseline';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <StyledEngineProvider injectFirst>
+      <CssBaseline />
       <App />
     </StyledEngineProvider>
   </React.StrictMode>
