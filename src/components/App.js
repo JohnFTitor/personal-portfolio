3ad75b99380c6edda057ab0ed88e38722648c0d3
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-04-17 16:41:59 -0500

    Define applciation color scheme

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,49 @@
 import '../App.css';
-import Button from '@mui/material/Button';
+//import Button from '@mui/material/Button';
+import { Box } from '@mui/material';
 
-function App() {
-  return (
+const App = () => (
     <div className="App">
       <header className="App-header">
-        <Button variant="contained" className="bg-indigo-500 font-extrabold"> Testing Material UI </Button>
+        <div className='flex'>
+          <Box sx={{ width: 300, height: 300 }} className= 'bg-primary-50'/>
+          <Box sx={{ width: 300, height: 300 }} className= 'bg-primary-100'/>
+          <Box sx={{ width: 300, height: 300 }} className= 'bg-primary-200'/>
+          <Box sx={{ width: 300, height: 300 }} className= 'bg-primary-300'/>
+          <Box sx={{ width: 300, height: 300 }} className= 'bg-primary-400'/>
+          <Box sx={{ width: 300, height: 300 }} className= 'bg-primary-500'/>
+          <Box sx={{ width: 300, height: 300 }} className= 'bg-primary-600'/>
+          <Box sx={{ width: 300, height: 300 }} className= 'bg-primary-700'/>
+          <Box sx={{ width: 300, height: 300 }} className= 'bg-primary-800'/>
+          <Box sx={{ width: 300, height: 300 }} className= 'bg-primary-900'/>
+          <Box sx={{ width: 300, height: 300 }} className= 'bg-primary-1000'/>
+        </div>
+        <div className='flex'>
+          <Box sx={{ width: 300, height: 300 }} className= 'bg-secondary-50'/>
+          <Box sx={{ width: 300, height: 300 }} className= 'bg-secondary-100'/>
+          <Box sx={{ width: 300, height: 300 }} className= 'bg-secondary-200'/>
+          <Box sx={{ width: 300, height: 300 }} className= 'bg-secondary-300'/>
+          <Box sx={{ width: 300, height: 300 }} className= 'bg-secondary-400'/>
+          <Box sx={{ width: 300, height: 300 }} className= 'bg-secondary-500'/>
+          <Box sx={{ width: 300, height: 300 }} className= 'bg-secondary-600'/>
+          <Box sx={{ width: 300, height: 300 }} className= 'bg-secondary-700'/>
+          <Box sx={{ width: 300, height: 300 }} className= 'bg-secondary-800'/>
+          <Box sx={{ width: 300, height: 300 }} className= 'bg-secondary-900'/>
+          <Box sx={{ width: 300, height: 300 }} className= 'bg-secondary-1000'/>
+        </div>
+        <div className='flex'>
+          <Box sx={{ width: 300, height: 300 }} className= 'bg-tertiary-50'/>
+          <Box sx={{ width: 300, height: 300 }} className= 'bg-tertiary-100'/>
+          <Box sx={{ width: 300, height: 300 }} className= 'bg-tertiary-200'/>
+          <Box sx={{ width: 300, height: 300 }} className= 'bg-tertiary-300'/>
+          <Box sx={{ width: 300, height: 300 }} className= 'bg-tertiary-400'/>
+          <Box sx={{ width: 300, height: 300 }} className= 'bg-tertiary-500'/>
+          <Box sx={{ width: 300, height: 300 }} className= 'bg-tertiary-600'/>
+          <Box sx={{ width: 300, height: 300 }} className= 'bg-tertiary-700'/>
+          <Box sx={{ width: 300, height: 300 }} className= 'bg-tertiary-800'/>
+          <Box sx={{ width: 300, height: 300 }} className= 'bg-tertiary-900'/>
+          <Box sx={{ width: 300, height: 300 }} className= 'bg-tertiary-1000'/>
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,7 +57,5 @@ function App() {
         </a>
       </header>
     </div>
-  );
-}
-
+);
 export default App;
