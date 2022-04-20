d983a7cd57f056774b826a3f46e8669734aabac4
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-04-19 23:01:47 -0500

    Add menu icon

--- src/components/App.js
+++ src/components/App.js
@@ -1,60 +1,10 @@
 import '../App.css';
-//import Button from '@mui/material/Button';
-import { Box } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const App = () => (
     <div className="App">
-      <header className="App-header">
-        <div className='flex'>
-          <Box sx={{ width: 300, height: 300 }} className= 'bg-primary-50'/>
-          <Box sx={{ width: 300, height: 300 }} className= 'bg-primary-100'/>
-          <Box sx={{ width: 300, height: 300 }} className= 'bg-primary-200'/>
-          <Box sx={{ width: 300, height: 300 }} className= 'bg-primary-300'/>
-          <Box sx={{ width: 300, height: 300 }} className= 'bg-primary-400'/>
-          <Box sx={{ width: 300, height: 300 }} className= 'bg-primary-500'/>
-          <Box sx={{ width: 300, height: 300 }} className= 'bg-primary-600'/>
-          <Box sx={{ width: 300, height: 300 }} className= 'bg-primary-700'/>
-          <Box sx={{ width: 300, height: 300 }} className= 'bg-primary-800'/>
-          <Box sx={{ width: 300, height: 300 }} className= 'bg-primary-900'/>
-          <Box sx={{ width: 300, height: 300 }} className= 'bg-primary-1000'/>
-        </div>
-        <div className='flex'>
-          <Box sx={{ width: 300, height: 300 }} className= 'bg-secondary-50'/>
-          <Box sx={{ width: 300, height: 300 }} className= 'bg-secondary-100'/>
-          <Box sx={{ width: 300, height: 300 }} className= 'bg-secondary-200'/>
-          <Box sx={{ width: 300, height: 300 }} className= 'bg-secondary-300'/>
-          <Box sx={{ width: 300, height: 300 }} className= 'bg-secondary-400'/>
-          <Box sx={{ width: 300, height: 300 }} className= 'bg-secondary-500'/>
-          <Box sx={{ width: 300, height: 300 }} className= 'bg-secondary-600'/>
-          <Box sx={{ width: 300, height: 300 }} className= 'bg-secondary-700'/>
-          <Box sx={{ width: 300, height: 300 }} className= 'bg-secondary-800'/>
-          <Box sx={{ width: 300, height: 300 }} className= 'bg-secondary-900'/>
-          <Box sx={{ width: 300, height: 300 }} className= 'bg-secondary-1000'/>
-        </div>
-        <div className='flex'>
-          <Box sx={{ width: 300, height: 300 }} className= 'bg-tertiary-50'/>
-          <Box sx={{ width: 300, height: 300 }} className= 'bg-tertiary-100'/>
-          <Box sx={{ width: 300, height: 300 }} className= 'bg-tertiary-200'/>
-          <Box sx={{ width: 300, height: 300 }} className= 'bg-tertiary-300'/>
-          <Box sx={{ width: 300, height: 300 }} className= 'bg-tertiary-400'/>
-          <Box sx={{ width: 300, height: 300 }} className= 'bg-tertiary-500'/>
-          <Box sx={{ width: 300, height: 300 }} className= 'bg-tertiary-600'/>
-          <Box sx={{ width: 300, height: 300 }} className= 'bg-tertiary-700'/>
-          <Box sx={{ width: 300, height: 300 }} className= 'bg-tertiary-800'/>
-          <Box sx={{ width: 300, height: 300 }} className= 'bg-tertiary-900'/>
-          <Box sx={{ width: 300, height: 300 }} className= 'bg-tertiary-1000'/>
-        </div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="flex p-2 px-3 border-2 border-red-500 border-solid fixed container">
+        <MenuIcon fontSize="large" className="text-primary-1000 text-5xl" />
       </header>
     </div>
 );
