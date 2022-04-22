2b7cf1cc5f822935edde7ceb1fef309b61eca7e8
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-04-21 21:22:24 -0500

    Move header to independent component

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,9 @@
 import '../App.css';
-import MenuIcon from '@mui/icons-material/Menu';
-import VolumeMuteIcon from '@mui/icons-material/VolumeMute';
-import { IconButton } from '@mui/material';
+import Header from './Header';
 
 const App = () => (
   <div className="App">
-    <header className="flex p-1 px-4 fixed container justify-between items-center">
-      <IconButton>
-        <MenuIcon fontSize="large" className="text-primary-1000 text-5xl" />
-      </IconButton>
-      <IconButton className="border-4 border-primary-1000 border-solid h-fit p-0">
-        <VolumeMuteIcon fontSize="large" className="text-primary-1000 text-4xl" />
-      </IconButton>
-    </header>
+    <Header />
   </div>
 );
 export default App;
