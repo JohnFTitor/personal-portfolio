436cc728f33e009e91cb02cdedaf2d3d4a05d3ab
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-05-28 17:59:43 -0500

    Fix bug when changing primary color

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,40 +7,40 @@ const Header = () => {
   const [menuActive, setMenu] = useState(false);
   const [animation, setAnimation] = useState('animate-slide_in');
   const [classes, setClasses] = useState({
-    color: '1000',
-    opacity: '100',
+    color: 'bg-primary-1000',
+    opacity: 'opacity-100',
     rotateUp: null,
     rotateDown: null,
   });
 
   const toggleMenu = () => {
     if (menuActive) {
+      setAnimation('animate-slide_out');
+      setTimeout(() => { setMenu(false); }, 700);
       setClasses({
-        color: '1000',
-        opacity: '100',
+        color: 'bg-primary-1000',
+        opacity: 'opacity-100',
         rotateUp: null,
         rotateDown: null,
       });
-      setAnimation('animate-slide_out');
-      setTimeout(() => { setMenu(false); }, 700);
     } else {
+      setAnimation('animate-slide_in');
+      setMenu(true);
       setClasses({
-        color: '50',
-        opacity: '0',
+        color: 'bg-primary-50',
+        opacity: 'opacity-0',
         rotateUp: 'rotate-up',
         rotateDown: 'rotate-down',
       });
-      setAnimation('animate-slide_in');
-      setMenu(true);
     }
   };
 
   return (
     <header className="flex p-2 px-4 w-full justify-between items-center">
       <IconButton onClick={toggleMenu} className="relative z-20 flex flex-col justify-between w-10 h-10 animate-slide_left gap-1">
-        <Box className={`bg-primary-${classes.color} h-1 w-8 origin-top-left transition-all ${classes.rotateDown}`} />
-        <Box className={`bg-primary-1000 h-1 w-8 transition-all opacity-${classes.opacity}`} />
-        <Box className={`bg-primary-${classes.color} h-1 w-8 origin-bottom-left transition-all ${classes.rotateUp}`} />
+        <Box className={`${classes.color} h-1 w-8 origin-top-left transition-all ${classes.rotateDown}`} />
+        <Box className={`bg-primary-1000 h-1 w-8 transition-all ${classes.opacity}`} />
+        <Box className={`${classes.color} h-1 w-8 origin-bottom-left transition-all ${classes.rotateUp}`} />
       </IconButton>
       <IconButton className="border-4 border-primary-1000 border-solid h-fit p-0 animate-slide_down">
         <VolumeMuteIcon fontSize="large" className="text-primary-1000 text-4xl" />
