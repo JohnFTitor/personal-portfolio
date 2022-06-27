de32b1746ed4874f4200cac42bd5fbd50386c84c
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-06-27 15:16:25 -0500

    Make project details responsive

--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -28,39 +28,41 @@ const ProjectDetails = () => {
   }, []);
 
   return (
-    <Box className="relative flex flex-col gap-8 items-center pt-5">
+    <Box className="relative flex flex-col gap-8 xl:gap-16 items-center pt-5 w-full px-2 2xl:px-10">
       <Box className="relative flex w-full px-5 justify-center items-center">
         <Link className="absolute left-2" data-aos="fade-right" to="/works">
           <ArrowForwardIosIcon className="text-primary-500 dark:text-primary-50 -rotate-180 text-3xl" />
         </Link>
-        <h1 className="text-3xl text-primary-1000 dark:text-white font-medium capitalize" data-aos="fade-up">{project.title}</h1>
+        <h1 className="text-3xl xl:text-4xl 2xl:text-5xl text-primary-1000 dark:text-white font-medium capitalize" data-aos="fade-up">{project.title}</h1>
       </Box>
-      <Box className="px-3" data-aos="fade">
+      <Box className="px-3 w-full flex justify-center max-w-2xl xl:max-w-4xl" data-aos="fade-right">
         <img src={project.image} alt={project.title} />
       </Box>
-      <p className="text-lg text-primary-1000 dark:text-white w-full px-5 mb-5" data-aos="fade-up">
-        {firstParagraph}
-      </p>
-      <Box className="px-3">
-        <img data-aos="flip-up" src={project.mobileImage} alt={project.title} className="animate-slide_up" />
+      <Box className="info grid gap-8 w-full">
+        <p className="first-paragraph text-lg 2xl:text-xl text-primary-1000 dark:text-white w-full px-5 mb-5" data-aos="fade-up">
+          {firstParagraph}
+        </p>
+        <Box className="mobile-image flex justify-center items-center mx-auto px-3 w-full max-w-2xl xl:max-w-4xl">
+          <img data-aos="fade-up" src={project.mobileImage} alt={project.title} className="animate-slide_up" />
+        </Box>
+        <Box className="extra-paragraphs w-full">
+          { (paragraphs.length > 0) && (
+            paragraphs.map((paragraph) => (
+              <p key={uuidv4()} className="text-lg 2xl:text-xl text-primary-1000 dark:text-white w-full mb-3 px-5" data-aos="fade-up">
+                {paragraph}
+              </p>
+            ))
+          )}
+        </Box>
       </Box>
-      <Box>
-        { (paragraphs.length > 0) && (
-          paragraphs.map((paragraph) => (
-            <p key={uuidv4()} className="text-lg text-primary-1000 dark:text-white w-full mb-3 px-5" data-aos="fade-up">
-              {paragraph}
-            </p>
-          ))
-        )}
-      </Box>
-      <ul className="flex flex-wrap w-full items-center justify-center gap-x-5 gap-y-2 px-3">
+      <ul className="flex flex-wrap w-full items-center justify-center gap-x-5 gap-y-2 mb-1 px-3">
         {project.tags.map((tag) => (
-          <li key={uuidv4()} className="text-xl text-primary-1000 dark:text-primary-50 font-medium border-solid border-2 border-primary-1000 dark:border-primary-50 py-0.5 px-2" data-aos="flip-down">{tag}</li>
+          <li key={uuidv4()} className="text-xl 2xl:text-2xl text-primary-1000 dark:text-primary-50 font-medium border-solid border-2 border-primary-1000 dark:border-primary-50 py-0.5 px-2 xl:px-5">{tag}</li>
         ))}
       </ul>
-      <Box className="w-full px-3">
+      <Box className="w-full max-w-7xl px-3">
         <iframe
-          data-aos="flip-up"
+          data-aos="fade-up"
           src={project.demo}
           title="YouTube video player"
           frameBorder="0"
@@ -68,17 +70,17 @@ const ProjectDetails = () => {
           allowFullScreen
         />
       </Box>
-      <Box className="flex gap-3 max-w-md justify-center flex-wrap w-full px-2 mb-7">
-        <a data-aos="flip-up" href={project.live} target="_blank" rel="noreferrer" className="text-white dark:text-primary-1000 flex-1 bg-primary-1000 dark:bg-primary-50 flex gap-5 items-center justify-between py-1 px-3 uppercase font-semibold">
-          <p className="text-lg">Live</p>
-          <WebIcon className="text-3xl text-white dark:text-primary-1000" />
+      <Box className="flex mx-auto xl:mx-0 gap-3 md:gap-20 max-w-md md:max-w-2xl justify-center flex-wrap mb-6 w-full px-2 xl:px-0">
+        <a href={project.live} target="_blank" rel="noreferrer" className="h-fit text-white dark:text-primary-1000 flex-1 bg-primary-1000 dark:bg-primary-50 flex gap-5 items-center justify-between py-1 xl:py-2 px-3 xl:px-5 uppercase font-semibold">
+          <p className="text-lg xl:text-2xl">Live</p>
+          <WebIcon className="text-3xl xl:text-5xl text-white dark:text-primary-1000" />
         </a>
-        <a data-aos="flip-down" href={project.source} target="_blank" rel="noreferrer" className="text-white dark:text-primary-1000 flex-1 bg-primary-1000 dark:bg-primary-50 flex gap-5 items-center justify-between py-1 px-3 uppercase font-semibold">
-          <p className="text-lg">Source</p>
-          <GitHubIcon className="text-3xl text-white dark:text-primary-1000" />
+        <a href={project.source} target="_blank" rel="noreferrer" className="h-fit text-white dark:text-primary-1000 flex-1 bg-primary-1000 dark:bg-primary-50 flex gap-5 items-center justify-between py-1 xl:py-2 px-3 xl:px-5 uppercase font-semibold">
+          <p className="text-lg xl:text-2xl">Source</p>
+          <GitHubIcon className="text-3xl xl:text-5xl text-white dark:text-primary-1000" />
         </a>
       </Box>
-      <Box className="w-full pb-3 pl-3">
+      <Box className="w-full p-3 md:pb-5 xl:pb-10">
         <ScrollUpButton />
       </Box>
     </Box>
