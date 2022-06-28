ff10d2fcfe4bc0667c75006635e3d9b704af080b
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-06-27 21:46:38 -0500

    Make form responsive

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,14 +12,14 @@ const Contact = () => {
   const [classes, setClasses] = useState({
     width: 'w-16 xl:w-24',
     display: 'block',
-    transform: 'hidden translate-y-full',
+    transform: 'hidden',
   });
 
   const openForm = () => {
     setClasses({
       width: 'w-0',
       display: 'hidden',
-      transform: 'flex translate-y-0',
+      transform: 'flex',
     });
   };
 
@@ -27,7 +27,7 @@ const Contact = () => {
     setClasses({
       width: 'w-16 xl:w-24',
       display: 'block',
-      transform: 'hidden translate-y-full',
+      transform: 'hidden',
     });
   };
 
@@ -42,17 +42,17 @@ const Contact = () => {
       <button type="button" onClick={openForm} className={`absolute flex bottom-0 right-0 h-16 ${width} xl:h-24 rounded-tl-full transition-all bg-primary-900`}>
         <span className={`${display} absolute text-xl xl:text-2xl text-white right-2 xl:right-4 bottom-2 xl:bottom-4 hover:scale-110 transition-transform`}> Hey </span>
       </button>
-      <Box component="form" className={`absolute right-0 bottom-0 transition-all ${transform} w-60 h-96 bg-secondary-50 border-l-2 border-t-2 border-primary-900 p-5 flex-col gap-5`}>
+      <Box component="form" className={`absolute right-0 bottom-0 transition-all ${transform} w-60 lg:w-72 h-96 xl:h-128 bg-secondary-50 border-l-2 border-t-2 border-primary-900 p-5 flex-col gap-5 overflow-auto`}>
         <div className="flex w-full justify-between items-center">
-          <span className="text-xl text-primary-900">Let&apos;s talk further!</span>
+          <span className="text-xl lg:text-2xl text-primary-900">Let&apos;s talk further!</span>
           <button type="button" onClick={closeForm}>
-            <CloseIcon className="text-primary-900" />
+            <CloseIcon className="text-primary-900 hover:scale-110 transition-transform" />
           </button>
         </div>
-        <Input type="text" placeholder="Enter your name" className="w-full" />
+        <Input type="text" placeholder="Enter your name" required className="w-full" />
         <Input type="email" placeholder="Enter your email" required className="w-full" />
-        <TextField multiline rows="4" placeholder="Would you like to say something?" className="w-full" />
-        <Button variant="contained" endIcon={<SendIcon />} className="bg-primary-900"> Send </Button>
+        <TextField multiline placeholder="Would you like to say something? :)" className="w-full" />
+        <Button type="submit" variant="contained" endIcon={<SendIcon />} className="bg-primary-900 hover:scale-105 transition-transform"> Send </Button>
       </Box>
     </>
   );
