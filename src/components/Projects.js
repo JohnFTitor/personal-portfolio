09662fea44070ec42df9da725bf4f6be70e14134
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-06-29 18:32:44 -0500

    Add async thunk to save the data from the API to the redux store

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,18 @@
 import { Box } from '@mui/material';
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Project from './Project';
+import { fetchProjects } from '../redux/projectsReducer';
 
 const Projects = () => {
-  const { data } = useSelector((state) => state.projects);
+  const { data, status } = useSelector((state) => state.projects);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (status === 'iddle') {
+      dispatch(fetchProjects());
+    }
+  });
 
   return (
     <Box className="w-full h-full overflow-hidden">
--- src/redux/projectsReducer.js
+++ src/redux/projectsReducer.js
@@ -1,21 +1,32 @@
+import { createReducer, createAsyncThunk } from '@reduxjs/toolkit';
+import getProjects from '../util/APIHandling';
+
 // Define initial state
-
-import { createAction, createReducer } from '@reduxjs/toolkit';
-
 const initialState = {
   data: [],
   status: 'iddle',
 };
 
 // Define Action
-const getProjects = createAction('GET_PROJECTS');
+const fetchProjects = createAsyncThunk(
+  'projects/fetchProjects',
+  async () => {
+    const data = await getProjects();
+    return data;
+  },
+);
 
-export { getProjects };
+export { fetchProjects };
 
 // Define Reducer
 const projectsReducer = createReducer(initialState, (builder) => {
   builder
-    .addCase(getProjects, (state) => state);
+    .addCase(fetchProjects.fulfilled, (state, action) => {
+      const newState = { ...state };
+      newState.status = 'completed';
+      newState.data = action.payload;
+      return newState;
+    });
 });
 
 export default projectsReducer;
